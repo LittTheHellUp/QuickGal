2f12cd1573db3401c487569c72a75e91349246f1
LittUp <LittTheHellUp8@gmail.com>
2015-04-29 21:55:50 +0100

    changed thumb width to 400 and added separator per dir

--- quickgal.py
+++ quickgal.py
@@ -5,26 +5,27 @@ from PIL import Image
 
 SCRIPT_PATH =  os.path.dirname(os.path.realpath(__file__)) + "/"
 IMAGE_FILE_REGEX = '^.+\.(png|jpg|jpeg|tif|tiff|gif|bmp)$'
-THUMBNAIL_WIDTH = 120
+THUMBNAIL_WIDTH = 400
 
 base_path = sys.argv[1]
 
 image_files = list()
 
+temp_root = "initial"
 for root, dirs, files in os.walk(base_path):
   for image in files:
     if re.match(IMAGE_FILE_REGEX, image): 
       try:
         im = Image.open(os.path.join(root,image))
+        if (temp_root != root) and (temp_root != "initial"):
+          image_files.append(["hr"])
         image_files.append([os.path.join(root,image),im.size])
       except:
         continue
+      temp_root = root
 if ( len(image_files) < 1 ):
 	print "No images found"
 	sys.exit(1)
-for image in image_files:
-  print os.path.basename(image[0]), " - ", str(image[1][0]) + 'x' + str(image[1][1])
-
 
 html = [
     '<html>',
@@ -42,9 +43,12 @@ html = [
     ]
 
 for image in image_files:
-  ratio = THUMBNAIL_WIDTH / float(image[1][0])
-  height = int(ratio * image[1][1])
-  html.append('       <a href="file:///'+image[0]+'" target="_blank"><img class="lazy" data-original="file:///'+image[0]+'" width="'+str(THUMBNAIL_WIDTH)+'" height="'+str(height)+'"></a>')
+  if image[0] == "hr":
+    html.append('       <br><hr><hr><br>')
+  else:
+    ratio = THUMBNAIL_WIDTH / float(image[1][0])
+    height = int(ratio * image[1][1])
+    html.append('       <a href="file:///'+image[0]+'" target="_blank"><img class="lazy" data-original="file:///'+image[0]+'" width="'+str(THUMBNAIL_WIDTH)+'" height="'+str(height)+'"></a>')
 
 html.extend(
     [
